bd5dad20c20fe9fdabc3f1886aba559842475932
vinayak3788 <vinayak3788@gmail.com>
2025-04-22 03:47:01 +0000

    Fix: Tailwind v4 postcss plugin issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-  plugins: [require('@tailwindcss/typography')]
-
-}
+};
